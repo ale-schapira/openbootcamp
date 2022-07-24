dd863dd678fcc7962a298360bf3a7ce21b260179
Ale Schapira <ale.schapira1@gmail.com>
2022-07-24 19:14:18 +0100

    ejercicio eslint

---
+++ eslint/index.js
@@ -0,0 +1,30 @@
+let name= "Alexia";
+let surname= "Schapira";
+let profesion= "developer"; 
+
+name;
+surname;
+profesion;
+
+// console.log(estudiante);
+
+// let estudianteMayus= estudiante.toUpperCase();
+// console.log(estudianteMayus);
+
+// let estudianteMinus= estudiante.toLocaleLowerCase();
+// console.log(estudianteMinus);
+
+// let estudianteLenght= estudiante.length;
+// console.log(estudianteLenght);
+
+// let firstLetter= estudiante.charAt(0);
+// console.log(firstLetter);
+
+// let lastLetter= estudiante.charAt(14);
+// console.log(lastLetter);
+
+// let spaceDelete= estudiante.trim();
+// console.log(spaceDelete)
+
+// let boolean= estudiante.includes("Alexia"); 
+// console.log(boolean);
